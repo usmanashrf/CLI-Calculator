2f36c6ae371bf97e8af65b9a0cb010aeb7bd381b
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2022-11-21 23:48:37 +0500

    create initial calculator structure

---
+++ src/modules/Result.js
@@ -0,0 +1,5 @@
+function Result(num1, num2, operation) {
+    let result = num1 + num2;
+    return result;
+}
+export default Result;
---
+++ src/modules/Result.ts
@@ -0,0 +1,8 @@
+
+function Result(num1:number, num2:number, operation:string){
+
+    let result = num1+num2;
+    return result;
+}
+
+export default Result;
